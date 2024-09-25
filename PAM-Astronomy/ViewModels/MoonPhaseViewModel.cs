46a007d474f1af228366acbce7d184617b574d20
andreysantoniello <andreysantoniello@gmail.com>
2024-09-25 18:06:55 -0300

    chore: base moon phae view model

--- PAM-Astronomy/ViewModels/MoonPhaseViewModel.cs
+++ PAM-Astronomy/ViewModels/MoonPhaseViewModel.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace PAM_Astronomy.ViewModels
 {
-    public class MoonPhaseViewModel
+    public partial class MoonPhaseViewModel : ObservableObject
     {
     }
 }
